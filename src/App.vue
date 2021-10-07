605e52adccf5ec43c825d978cfb0d2521a5e2005
xiaozou <1424113865@qq.com>
2021-10-07 22:19:45 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>123</div>
+  <Frank/>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
