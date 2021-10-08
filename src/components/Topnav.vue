0d7a105dedc00c518174716ca62eed827be7bb02
xiaozou <1424113865@qq.com>
2021-10-08 15:10:24 +0800

    完善 aside 切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,6 +6,7 @@
                 <li>菜单1</li>
                 <li>菜单2</li>
             </ul>
+            <span class="toggleAside" @click="toggleAside"></span>
         </div>
     </div>
 </template>
@@ -30,6 +31,8 @@ export default {
     padding: 16px;
     position: relative;
     z-index: 10;
+    justify-content: center;
+    align-items: center;
     > .logo {
         max-width: 6em;
         margin-right: auto;
@@ -41,6 +44,21 @@ export default {
         > li {
         margin: 0 1em;
         }
+    }
+    > .toggleAside {
+        width: 24px;
+        height: 24px;
+        background: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+        display: none;
+    }
+    @media(max-width:500px){
+        > .menu{display: none;}
+        > .logo{margin: 0 auto;}
+        > .toggleAside{display: inline-block;}
     }
 }
 </style> 
