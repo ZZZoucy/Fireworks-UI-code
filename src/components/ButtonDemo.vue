48ce175ac9840ca92968a8ba4c07607951985981
xiaozou <1424113865@qq.com>
2021-10-12 00:02:59 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,4 +2,15 @@
     <div>
         ButtonDemo
     </div>
+    <h1>示例</h1>
+    <div>
+        <Button>你好</Button>
+    </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+    components:{ Button },
+}
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default {
+    
+}
+</script>
