f91ddd8d17652d122f132dad70f3b4a06cab0ef4
xiaozou <1424113865@qq.com>
2021-10-16 23:53:13 +0800

    完成 button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,7 +4,7 @@
     </div> 
     <h1>示例1</h1>
     <div>
-        <Button>你好</Button>
+        <Button @click="onClick">你好</Button>
         <Button theme="button">你好</Button>
         <Button theme="link">你好</Button>
         <Button theme="text">你好</Button>
@@ -50,6 +50,7 @@
     <h1>示例5</h1>
     <div>
         <Button loading>加载中</Button>
+        <Button>加载完毕</Button>
     </div>
 </template>
 
