e5a0711dd6b7be7988a82039df7f5ecb516a02fe
xiaozou <1424113865@qq.com>
2021-10-16 23:47:00 +0800

    支持 loading 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -45,7 +45,12 @@
     <div>
         <Button disabled>禁用按钮</Button>
         <Button theme="link" disabled>禁用链接按钮</Button>
-        <Button theme="text" disabled>禁用按钮</Button>    </div>
+        <Button theme="text" disabled>禁用按钮</Button>    
+    </div>
+    <h1>示例5</h1>
+    <div>
+        <Button loading>加载中</Button>
+    </div>
 </template>
 
 <script lang="ts">
