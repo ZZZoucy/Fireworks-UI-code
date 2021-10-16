b99b18f90fbf774301d52a889f35a3e48b751f19
xiaozou <1424113865@qq.com>
2021-10-17 00:09:50 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        DialogDemo
+        DialogDemo 
     </div>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+    components: {
+        Dialog
+    }
+}
+</script>
