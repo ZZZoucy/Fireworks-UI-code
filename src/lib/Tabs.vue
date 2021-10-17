1b74510cb74cba9f9326ad2dd79e1b13c0f4c126
xiaozou <1424113865@qq.com>
2021-10-17 19:38:38 +0800

    优化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="gulu-tabs">
         <div class="gulu-tabs-nav" ref="container">
-            <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => {if (el) navItems[index] = el}" @click="select(t)" :class="{selected:t===selected}" :key="index">{{t}}</div>
-            <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
-        </div>
-        <div class="gulu-tabs-content">
-            <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" :key="c"/>
-        </div>
+        <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (el) navItems[index] = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+        <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="gulu-tabs-content">
+        <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" :key="c"/>
+    </div>
     </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
-import { computed, onMounted, onUpdated, ref } from 'vue';
+import { computed, ref, onMounted,onUpdated } from 'vue';
 export default {
     props:{
         selected:{
@@ -20,16 +20,16 @@ export default {
         }
     },
     setup(props,context){
-        const navItems = ref<HTMLDivElement[]>([])
+        const navItems = ref <HTMLDivElement[]>([])
         const indicator = ref<HTMLDivElement>(null)
         const container = ref<HTMLDivElement>(null)
         const x = () => {
             const divs = navItems.value
             const result = divs.filter(div => div.classList.contains('selected'))[0]
-            const {width} = result.getBoundingClientRect()
+            const { width } = result.getBoundingClientRect()
             indicator.value.style.width = width + 'px'
-            const {left:left1} = container.value.getBoundingClientRect()
-            const {left:left2} = result.getBoundingClientRect()
+            const { left: left1 } = container.value.getBoundingClientRect()
+            const { left: left2 } = result.getBoundingClientRect()
             const left = left2 - left1
             indicator.value.style.left = left + 'px'
         }
@@ -41,18 +41,13 @@ export default {
                 throw new Error('Tabs 子标签必须是 Tab')
             }
         })
-        const current = computed(()=>{
-            return defaults.filter((tag)=>{
-                return tag.props.title === props.selected
-            })[0]
-        })
         const titles = defaults.map((tag)=>{
             return tag.props.title
         })
         const select = (title: String) => {
             context.emit('update:selected', title)
         }
-        return { defaults,titles,current,select,navItems,indicator,container }
+        return { defaults,titles,select,navItems,indicator,container }
     }
 }
 </script>
