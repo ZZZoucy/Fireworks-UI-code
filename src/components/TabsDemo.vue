d2270e82552afca3fc12a0c53b06081ea4fc7611
xiaozou <1424113865@qq.com>
2021-10-17 19:05:37 +0800

    实现下划线滑动

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
         TabsDemo
     </div>
     <h1>示例1</h1>
-    <Tabs>
+    <Tabs v-model:selected="x">
         <Tab title="导航1">内容1</Tab>
         <Tab title="导航2">内容2</Tab>
     </Tabs>
@@ -12,7 +12,12 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue'
+import { ref } from 'vue';
 export default {
-    components:{ Tabs, Tab }
+    components:{ Tabs, Tab },
+    setup(){
+        const x = ref('导航1')
+        return { x }
+    }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,28 +1,58 @@
 <template>
     <div class="gulu-tabs">
-        <div class="gulu-tabs-nav">
-            <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+        <div class="gulu-tabs-nav" ref="container">
+            <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => {if (el) navItems[index] = el}" @click="select(t)" :class="{selected:t===selected}" :key="index">{{t}}</div>
+            <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
         </div>
         <div class="gulu-tabs-content">
-            <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+            <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" :key="c"/>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from './Tab.vue';
+import { computed, onMounted, onUpdated, ref } from 'vue';
 export default {
+    props:{
+        selected:{
+            type: String
+        }
+    },
     setup(props,context){
+        const navItems = ref<HTMLDivElement[]>([])
+        const indicator = ref<HTMLDivElement>(null)
+        const container = ref<HTMLDivElement>(null)
+        const x = () => {
+            const divs = navItems.value
+            const result = divs.filter(div => div.classList.contains('selected'))[0]
+            const {width} = result.getBoundingClientRect()
+            indicator.value.style.width = width + 'px'
+            const {left:left1} = container.value.getBoundingClientRect()
+            const {left:left2} = result.getBoundingClientRect()
+            const left = left2 - left1
+            indicator.value.style.left = left + 'px'
+        }
+        onMounted(x)
+        onUpdated(x)
         const defaults = context.slots.default()
         defaults.forEach((tag)=>{
             if(tag.type !== Tab){
                 throw new Error('Tabs 子标签必须是 Tab')
             }
         })
+        const current = computed(()=>{
+            return defaults.filter((tag)=>{
+                return tag.props.title === props.selected
+            })[0]
+        })
         const titles = defaults.map((tag)=>{
             return tag.props.title
         })
-        return { defaults,titles }
+        const select = (title: String) => {
+            context.emit('update:selected', title)
+        }
+        return { defaults,titles,current,select,navItems,indicator,container }
     }
 }
 </script>
@@ -36,6 +66,7 @@ export default {
             display: flex;
             color: $color;
             border-bottom: 1px solid $border-color;
+            position: relative;
             &-item{
                 padding: 8px 0;
                 margin: 0 16px;
@@ -47,9 +78,24 @@ export default {
                     color: $blue;
                 }
             }
+            &-indicator{
+                position: absolute;
+                height: 3px;
+                background: $blue;
+                left: 0;
+                bottom: -1px;
+                width: 100px;
+                transition: all 250ms;
+            }
         }
         &-content{
             padding: 8px 0;
+            &-item {
+                display: none;
+                &.selected {
+                    display: block;
+                }
+            }
         }
     }
 </style>
