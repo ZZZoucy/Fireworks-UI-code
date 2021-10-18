63808846b704a41356911b9ada5986d116630eaa
xiaozou <1424113865@qq.com>
2021-10-18 17:17:33 +0800

    优化Markdown

--- src/router.ts
+++ src/router.ts
@@ -1,35 +1,32 @@
 import { createWebHashHistory, createRouter } from "vue-router";
-import Home from "./views/Home.vue";
-import Doc from "./views/Doc.vue";
-import SwitchDemo from "./components/SwitchDemo.vue";
-import ButtonDemo from "./components/ButtonDemo.vue";
-import DialogDemo from "./components/DialogDemo.vue";
-import TabsDemo from "./components/TabsDemo.vue";
-
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
+import SwitchDemo from './components/SwitchDemo.vue';
+import ButtonDemo from './components/ButtonDemo.vue';
+import DialogDemo from './components/DialogDemo.vue';
+import TabsDemo from './components/TabsDemo.vue';
+import DocDemo from './components/DocDemo.vue';
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
+
 const history = createWebHashHistory();
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = filename => h(Markdown,{path:`../Markdown/${filename}.md`,key:filename })
 export const router = createRouter({
-  history: history,
-  routes: [
-    { path: "/", component: Home },
-    {
-      path: "/doc",
-      component: Doc,
-      children: [
-        { path: "", redirect: '/doc/intro' },
-        { path: "intro", component: md('intro') },
-        { path: "get-started", component: md('get-started') },
-        { path: "install", component: md('install') },
-        { path: "switch", component: SwitchDemo },
-        { path: "button", component: ButtonDemo },
-        { path: "dialog", component: DialogDemo },
-        { path: "tabs", component: TabsDemo },
-      ],
-    },
-  ],
+    history:history,
+    routes:[
+        {path:'/',component:Home},
+        {path:'/doc',component: Doc,children:[
+            {path:'',component: DocDemo},
+            {path:"intro",component: md('intro') },
+            {path:"get-started",component: md('get-started') },
+            {path:"install",component: md('install') },
+            {path:'switch',component: SwitchDemo},
+            {path:'button',component: ButtonDemo},
+            {path:'dialog',component: DialogDemo},
+            {path:'tabs',component: TabsDemo}
+        ]}
+    ]
 });
-router.afterEach(() => {
-  console.log("路由切换了");
-});
+router.afterEach(()=>{
+    
+})
