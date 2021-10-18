88fb592cd7a00532234581eb2689bf53eff4560e
xiaozou <1424113865@qq.com>
2021-10-18 21:33:07 +0800

    源代码高亮

--- src/components/Switch2.demo.vue
+++ src/components/Switch2.demo.vue
@@ -6,9 +6,9 @@
 </template>
 
 <script lang="ts">
-  import Switch from '../lib/Switch.vue'
-  import { ref } from 'vue'
-  export default {
+import Switch from '../lib/Switch.vue'
+import { ref } from 'vue'
+export default {
   components: { Switch },
   setup() {
     const bool = ref(false)
