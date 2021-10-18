e78705e525557159ca9500014461e244dd6f85b8
xiaozou <1424113865@qq.com>
2021-10-19 00:20:06 +0800

    优化

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -43,6 +43,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$aside-index: 10;
 .layout {
     display: flex;
     flex-direction: column;
@@ -79,6 +80,7 @@ aside {
     left: 0;
     padding-top: 70px;
     height: 100%;
+    z-index: $aside-index;
     > h2 {
         margin-bottom: 4px;
         padding: 0 16px;
