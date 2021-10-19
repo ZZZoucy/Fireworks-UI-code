ad7be05b4e0f78228f08a31041105e6b4888636f
xiaozou <1424113865@qq.com>
2021-10-19 10:08:40 +0800

    尝试解决bug

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,31 +1,32 @@
 <template>
     <div class="layout">
-    <Topnav toggleMenuButtonVisible class="nav" />
-    <div class="content">
-        <aside v-if="asideVisible">
-            <h2>文档</h2>
-            <ol>
-                <li>
-                    <router-link to="/doc/intro">介绍</router-link>
-                </li>
-                <li>
-                    <router-link to="/doc/install">安装</router-link>
-                </li>
-                <li>
-                    <router-link to="/doc/get-started">开始使用</router-link>
-                </li>
-            </ol>
-            <h2>组件列表</h2>
-            <ol>
-                <li><router-link to="/doc/switch">Switch 组件</li>
-                <li><router-link to="/doc/button">Button 组件</li>
-                <li><router-link to="/doc/dialog">Dialog 组件</li>
-                <li><router-link to="/doc/tabs">Tabs 组件</li>
-            </ol>
-        </aside>
-        <main>
-            <router-view />
-        </main>
+        <Topnav toggleMenuButtonVisible class="nav" />
+        <div class="content">
+            <aside v-if="asideVisible">
+                <h2>文档</h2>
+                <ol>
+                    <li>
+                        <router-link to="/doc/intro">介绍</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/install">安装</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/get-started">开始使用</router-link>
+                    </li>
+                </ol>
+                <h2>组件列表</h2>
+                <ol>
+                    <li><router-link to="/doc/switch">Switch 组件</router-link></li>
+                    <li><router-link to="/doc/button">Button 组件</router-link></li>
+                    <li><router-link to="/doc/dialog">Dialog 组件</router-link></li>
+                    <li><router-link to="/doc/tabs">Tabs 组件</router-link></li>
+                </ol>
+            </aside>
+            <main>
+                <router-view />
+            </main>
+        </div>
     </div>
 </template>
 
