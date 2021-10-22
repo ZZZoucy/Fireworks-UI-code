54f8c5fd9fbed64b120ca9f31d96e8294a10b15b
xiaozou <1424113865@qq.com>
2021-10-22 14:42:02 +0800

    update

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,58 +1,60 @@
 <template>
-    <div class="gulu-tabs">
-        <div class="gulu-tabs-nav" ref="container">
-            <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
-            <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
-        </div>
-        <div class="gulu-tabs-content">
-            <component :is="current" :key="current.props.title" />
-        </div>
-    </div>
+<div class="gulu-tabs">
+  <div class="gulu-tabs-nav" ref="container">
+    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+    <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+  </div>
+  <div class="gulu-tabs-content">
+    <component :is="current" :key="current.props.title" />
+  </div>
+</div>
 </template>
 
 <script lang="ts">
-import Tab from './Tab.vue';
-import { computed, ref, onMounted,onUpdated } from 'vue';
+import Tab from './Tab.vue'
+import {
+  computed,
+  ref,
+  watchEffect,
+  onMounted
+} from 'vue'
 export default {
-    props:{
-        selected:{
-            type: String
-        }
-    },
-    setup(props,context){
-        const navItems = ref <HTMLDivElement[]>([])
-        const indicator = ref<HTMLDivElement>(null)
-        const container = ref<HTMLDivElement>(null)
-        const x = () => {
-            const divs = navItems.value
-            const result = divs.filter(div => div.classList.contains('selected'))[0]
-            const { width } = result.getBoundingClientRect()
-            indicator.value.style.width = width + 'px'
-            const { left: left1 } = container.value.getBoundingClientRect()
-            const { left: left2 } = result.getBoundingClientRect()
-            const left = left2 - left1
-            indicator.value.style.left = left + 'px'
-        }
-        onMounted(x)
-        onUpdated(x)
-        const defaults = context.slots.default()
-        defaults.forEach((tag)=>{
-            // @ts-ignore
-            if (tag.type.name !== Tab.name) {
-                throw new Error('Tabs 子标签必须是 Tab')
-            }
-        })
-        const current = computed(() => {
-            return defaults.find(tag => tag.props.title === props.selected)
-        })
-        const titles = defaults.map((tag)=>{
-            return tag.props.title
-        })
-        const select = (title: String) => {
-            context.emit('update:selected', title)
-        }
-        return { current,defaults,titles,select,navItems,indicator,container }
+  props: {
+    selected: {
+      type: String
     }
+  },
+  setup(props, context) {
+    const selectedItem = ref < HTMLDivElement > (null)
+    const indicator = ref < HTMLDivElement > (null)
+    const container = ref < HTMLDivElement > (null)
+    onMounted(() => {
+      watchEffect(() => {
+        const { width } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const { left: left1 } = container.value.getBoundingClientRect()
+        const { left: left2 } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab')
+      }
+    })
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
+    return { current,defaults,titles,select,selectedItem,indicator,container }
+  }
 }
 </script>
 
